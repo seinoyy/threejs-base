ee2cf880333daaa80b5aa087fcb117239822dd96
彭文耀 <pengwenyao@huiyitech.com.cn>
2026-05-21 10:51:15 +0800

    修改原因：完成鬼屋练习

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-import './style.css'
+import '../css/style.css'
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
