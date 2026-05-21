0822f642dfcc93d003fa5309de09fb3e190c959d
彭文耀 <pengwenyao@huiyitech.com.cn>
2026-05-21 14:50:53 +0800

    提交原因：完成粒子课程

---
+++ src/js/particle.js
@@ -0,0 +1,120 @@
+import '../css/style.css'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui"
+
+// 创建gui
+const gui = new GUI({
+  width: 300,
+  title: 'Nice debug UI',
+  closeFolders: false
+})
+
+// 获取canvas标签
+const canvas = document.querySelector("canvas.webgl")
+
+// 创建场景
+const scene = new THREE.Scene()
+
+// 创建纹理
+// 创建纹理加载器
+const textureLoader = new THREE.TextureLoader()
+
+const particleTexture = textureLoader.load('../../static/particles/2.png')
+
+// 创建粒子
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 20000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10
+  colors[i] = Math.random()
+}
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+const particlesMaterial = new THREE.PointsMaterial()
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true
+// particlesMaterial.color = new THREE.Color(0xff00ff)
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial()
+)
+// scene.add(cube)
+
+// 三维空间大小
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// 创建控制器
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// 动画
+// 创建时钟
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update particles
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3
+    const x = particlesGeometry.attributes.position.array[i3]
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+  }
+  particlesGeometry.attributes.position.needsUpdate = true
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
