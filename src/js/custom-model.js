8d765eac7cad64777019fa330b791d4bf50f1829
彭文耀 <pengwenyao@huiyitech.com.cn>
2026-05-27 11:31:01 +0800

    提交原因：学习使用blender

---
+++ src/js/custom-model.js
@@ -0,0 +1,123 @@
+import '../css/style.css'
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import GUI from "lil-gui"
+
+// 创建gui
+const gui = new GUI({
+  width: 300,
+  title: 'Nice debug UI',
+  closeFolders: false
+})
+
+// 获取canvas标签
+const canvas = document.querySelector("canvas.webgl")
+
+// 创建场景
+const scene = new THREE.Scene()
+
+// 创建模型
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('../../static/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
+
+gltfLoader.load(
+  '../../static/models/hamburger.glb',
+  (gltf) => {
+    scene.add(gltf.scene)
+  }
+)
+
+// 创建地面
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(50, 50),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5
+  })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+// 创建光源
+// 环境光
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+scene.add(ambientLight)
+// 平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+// 三维空间大小
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(- 8, 4, 8)
+scene.add(camera)
+
+// 创建控制器
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 1, 0)
+controls.enableDamping = true
+
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// 动画
+const timer = new THREE.Timer()
+
+const tick = () => {
+  timer.update()
+  const elapsedTime = timer.getElapsed()
+  const deltaTime = timer.getDelta()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
