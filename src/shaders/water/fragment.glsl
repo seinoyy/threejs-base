1d0cc6ad4ed2fa084a0f8a12801cfed2e0182b01
彭文耀 <pengwenyao@huiyitech.com.cn>
2026-06-11 16:33:02 +0800

    提交原因：使用着色器加入灯光优化海洋效果

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -4,10 +4,30 @@ uniform float uColorOffset;
 uniform float uColorMultiplier;
 
 varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/directionalLight.glsl
+#include ../includes/pointLight.glsl
 
 void main() {
+  vec3 normal = normalize(vNormal);
+  vec3 viewDirection = normalize(vPosition - cameraPosition);
+
+  // base
   float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+  mixStrength = smoothstep(0.0, 1.0, mixStrength);
   vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
 
+  // light
+  vec3 light = vec3(0.0);
+  // light += directionalLight(vec3(1.0), 1.0, normal, vec3(-1.0, 0.5, 0.0), viewDirection, 30.0);
+  light += pointLight(vec3(1.0), 10.0, normal, vec3(0.0, 0.25, 0.0), viewDirection, 30.0, vPosition, 0.95);
+
+  color *= light;
+
+  // final color
   gl_FragColor = vec4(color, 1.0);
+  #include <tonemapping_fragment>
+  #include <colorspace_fragment>
 }
