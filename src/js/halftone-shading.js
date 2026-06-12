82190a76296f2bbd9ae44551a2776eef17e7d95d
彭文耀 <pengwenyao@huiyitech.com.cn>
2026-06-12 11:27:32 +0800

    提交原因：使用着色器编写半色调效果

---
+++ src/js/halftone-shading.js
@@ -0,0 +1,188 @@
+import '../css/style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import GUI from 'lil-gui'
+import halftoneVertexShader from '@/shaders/halftone/vertex.glsl'
+import halftoneFragmentShader from '@/shaders/halftone/fragment.glsl'
+
+// 创建gui
+const gui = new GUI()
+
+// 获取canvas标签
+const canvas = document.querySelector("canvas.webgl")
+
+// 创建场景
+const scene = new THREE.Scene()
+
+// 创建加载器
+const gltfLoader = new GLTFLoader()
+
+// 三维空间大小
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2)
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+  // Update material
+  material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(sizes.pixelRatio)
+})
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(7, 7, 7)
+scene.add(camera)
+
+// 创建控制器
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// 创建渲染器
+const rendererParameters = {}
+rendererParameters.clearColor = '#26132f'
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true
+})
+renderer.setClearColor(rendererParameters.clearColor)
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(sizes.pixelRatio)
+
+gui
+  .addColor(rendererParameters, 'clearColor')
+  .onChange(() => {
+    renderer.setClearColor(rendererParameters.clearColor)
+  })
+
+/**
+* Material
+*/
+const materialParameters = {}
+materialParameters.color = '#ff794d'
+materialParameters.shadowColor = '#8e19b8'
+materialParameters.lightColor = '#e5ffe0'
+
+const material = new THREE.ShaderMaterial({
+  vertexShader: halftoneVertexShader,
+  fragmentShader: halftoneFragmentShader,
+  uniforms:
+  {
+    uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
+    uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+    uShadowRepetitions: new THREE.Uniform(100),
+    uShadowColor: new THREE.Uniform(new THREE.Color(materialParameters.shadowColor)),
+    uLightRepetitions: new THREE.Uniform(130),
+    uLightColor: new THREE.Uniform(new THREE.Color(materialParameters.lightColor))
+  }
+})
+
+gui
+  .addColor(materialParameters, 'color')
+  .onChange(() => {
+    material.uniforms.uColor.value.set(materialParameters.color)
+  })
+
+gui
+  .add(material.uniforms.uShadowRepetitions, 'value')
+  .min(1)
+  .max(300)
+  .step(1)
+
+gui
+  .addColor(materialParameters, 'shadowColor')
+  .onChange(() => {
+    material.uniforms.uShadowColor.value.set(materialParameters.shadowColor)
+  })
+
+gui
+  .add(material.uniforms.uLightRepetitions, 'value')
+  .min(1)
+  .max(300)
+  .step(1)
+
+gui
+  .addColor(materialParameters, 'lightColor')
+  .onChange(() => {
+    material.uniforms.uLightColor.value.set(materialParameters.lightColor)
+  })
+
+/**
+ * Objects
+ */
+// Torus knot
+const torusKnot = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
+  material
+)
+torusKnot.position.x = 3
+scene.add(torusKnot)
+
+// Sphere
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  material
+)
+sphere.position.x = - 3
+scene.add(sphere)
+
+// Suzanne
+let suzanne = null
+gltfLoader.load(
+  '../../static/models/suzanne.glb',
+  (gltf) => {
+    suzanne = gltf.scene
+    suzanne.traverse((child) => {
+      if (child.isMesh)
+        child.material = material
+    })
+    scene.add(suzanne)
+  }
+)
+
+// 动画
+const timer = new THREE.Timer()
+
+const tick = () => {
+  timer.update()
+  const elapsedTime = timer.getElapsed()
+  const deltaTime = timer.getDelta()
+
+  // Rotate objects
+  if (suzanne) {
+    suzanne.rotation.x = - elapsedTime * 0.1
+    suzanne.rotation.y = elapsedTime * 0.2
+  }
+
+  sphere.rotation.x = - elapsedTime * 0.1
+  sphere.rotation.y = elapsedTime * 0.2
+
+  torusKnot.rotation.x = - elapsedTime * 0.1
+  torusKnot.rotation.y = elapsedTime * 0.2
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
