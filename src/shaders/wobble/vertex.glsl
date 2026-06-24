497524bbdb81c3bae12b2d438c207e23cb5e57ff
彭文耀 <pengwenyao@huiyitech.com.cn>
2026-06-24 18:00:34 +0800

    提交原因：给内置材质编写着色器实现

---
+++ src/shaders/wobble/vertex.glsl
@@ -0,0 +1,43 @@
+uniform float uTime;
+uniform float uPositionFrequency;
+uniform float uTimeFrequency;
+uniform float uStrength;
+uniform float uWarpPositionFrequency;
+uniform float uWarpTimeFrequency;
+uniform float uWarpStrength;
+
+attribute vec4 tangent;
+
+varying float vWobble;
+
+#include ../includes/simplexNoise4d.glsl
+
+float getWobble(vec3 position) {
+  vec3 warpedPosition = position;
+  warpedPosition += simplexNoise4d(vec4(position * uWarpPositionFrequency, uTime * uWarpTimeFrequency)) * uWarpStrength;
+
+  return simplexNoise4d(vec4(warpedPosition * uPositionFrequency, uTime * uTimeFrequency)) * uStrength;
+}
+
+void main() {
+  vec3 biTangent = cross(normal, tangent.xyz);
+
+  // Neighbours position
+  float shift = 0.01;
+  vec3 positionA = csm_Position + shift * tangent.xyz;
+  vec3 positionB = csm_Position + shift * biTangent;
+
+  // Wobble
+  float wobble = getWobble(csm_Position);
+  csm_Position += wobble * normal;
+  positionA += getWobble(positionA) * normal;
+  positionB += getWobble(positionB) * normal;
+
+  // Compute normal
+  vec3 toA = normalize(positionA - csm_Position);
+  vec3 toB = normalize(positionB - csm_Position);
+  csm_Normal = cross(toA, toB);
+
+  // Varying
+  vWobble = wobble / uStrength;
+}
